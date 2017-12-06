f20801578777dc52e577d61be4605eaf7d722954
Morgan <heshammse@gmail.com>
2017-12-06 14:26:43 -0800

    fixed layout

--- php/login.php
+++ php/login.php
@@ -1,15 +1,19 @@
 <?php
 require("../helper/sqlExec.php");
 session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-//$username = 'Soldier';
-//$password = 'first';
-//$result = sqlExec("Exec login_user @username= $username , @password = $password");
-$_SESSION['userid'] = 'Soldier';
-//print($_SESSION["userid"]);
-//print("</br>");
-$result2 = sqlExec("SELECT  * FROM Users WHERE username = '".$_SESSION['userid']."'");
-print_r($result2);
-//header("Location: /php/staff member.php");
-//exit();/// exit whatever data
+$result = sqlExec("Exec login_user @username= $username , @password = $password");
+if(empty($result)){
+  echo "FUCK YOU!!!!!! Fuck OFFF!!";
+}else{
+  $_SESSION['userid'] = $_POST['username'];
+  $_SESSION['position'] = sqlExec("Exec Find_Type @username= $username")[0][0];
+  header("Location: /layout/mainlayout.html");
+  exit();
+}
+
+
+
 ?>
