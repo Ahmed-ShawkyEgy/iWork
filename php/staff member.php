5bbe233661030be59816e1c2cf3a70cf0b905d59
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-12-07 00:51:59 +0200

    further removed redundunt temp files

--- php/staff member.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-echo $_SESSION['userid'];
-if($_SESSION==null or $_SESSION['type']!='staff member')
-{
-   header("Location: /layout/Appology/user-appology.html");
-   exit();
-}
-?>
