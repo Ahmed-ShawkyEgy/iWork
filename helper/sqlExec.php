58b68863fe98d893ab883a877fa041b1cbde9ba4
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-12-07 00:31:13 +0200

    fixed beutify error

--- helper/sqlExec.php
+++ helper/sqlExec.php
@@ -1,21 +1,67 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT']."/connection/databaseConnection.php");
+
+require("../connection/databaseConnection.php");
 
 sqlsrv_configure("WarningsReturnAsErrors", 0);
-function print_row(&$item) {
-  echo('<tr>');
-  array_walk($item, 'print_cell');
-  echo('</tr>');
+
+/* ------------- Error Handling Functions --------------*/
+function DisplayErrors()
+{
+     $errors = sqlsrv_errors(SQLSRV_ERR_ERRORS);
+     foreach( $errors as $error )
+     {
+         echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+         echo "code: ".$error[ 'code']."<br />";
+         echo "Error: ".$error['message']."\n";
+     }
 }
 
-function print_cell(&$item) {
-    
-  echo('<td>');
-  echo($item);
-  echo('</td>');
+function DisplayWarnings()
+{
+     $warnings = sqlsrv_errors(SQLSRV_ERR_WARNINGS);
+     if(!is_null($warnings))
+     {
+          foreach( $warnings as $warning )
+          {
+               echo "SQLSTATE: ".$warnings[ 'SQLSTATE']."<br />";
+               echo "code: ".$warnings[ 'code']."<br />";
+               echo "Warning: ".$warning['message']."\n";
+          }
+     }
 }
 
-function echoTable(){
-   
-    echo "<table><?php array_walk(".$data.", 'print_row');?>
-    </table>"; } function DisplayErrors() { $errors = sqlsrv_errors(SQLSRV_ERR_ERRORS); foreach( $errors as $error ) { echo "Error: ".$error['message']."\n"; } } function DisplayWarnings() { $warnings = sqlsrv_errors(SQLSRV_ERR_WARNINGS); if(!is_null($warnings)) { foreach( $warnings as $warning ) { echo "Warning: ".$warning['message']."\n"; } } } /* ------------- Mssql Execution Function --------------*/ function sqlExec($sql){ $result = []; $stmt = sqlsrv_query($GLOBALS['conn'], $sql,array(),array( "Scrollable" => 'static' )); if(!$stmt) { die( print_r( sqlsrv_errors(), true)); } while ($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC)) { $result[] = $row; } $next_result = sqlsrv_next_result($stmt); while($next_result){ while ($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC)) { $result[] = $row; } $next_result = sqlsrv_next_result($stmt); } //DisplayErrors(); //DisplayWarnings(); return $result; } ?>
+/* ------------- Mssql Execution Function --------------*/
+function sqlExec($sql){
+
+     $result = [];
+
+     $stmt = sqlsrv_query($GLOBALS['conn'], $sql,array(),array( "Scrollable" => 'static' ));
+
+     if(!$stmt) {
+        die( print_r( sqlsrv_errors(), true));
+     }
+
+     while ($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC))
+     {
+       $result[] = $row;
+     }
+
+	 $next_result = sqlsrv_next_result($stmt);
+
+     while($next_result){
+
+			 while ($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC))
+             {
+               $result[] = $row;
+             }
+
+			 $next_result = sqlsrv_next_result($stmt);
+	 }
+      //DisplayErrors();
+    //  DisplayWarnings();
+
+    return $result;
+
+}
+
+?>
