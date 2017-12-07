88efe7988fe3bd3a067f739f47205fec7a509bf2
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-12-08 01:13:17 +0200

    fixed bugs & finalized email semantics

---
+++ layout/Staff Member/send-email.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>View Requests</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../style/appology.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+</head>
+
+<body>
+
+    <?php     
+// Staff Member view his/her requests
+// TODO create front end form
+// TODO create front end display
+require_once($_SERVER['DOCUMENT_ROOT']."/Database-Project/php/navbar.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/Database-Project/helper/sqlExec.php");
+if(session_status() == PHP_SESSION_NONE)
+session_start();
+
+    
+// TODO remove this line
+$_SESSION['userid'] = "Trissy";
+// TODO add Session auth ie if($session == null) etc
+//if($_SESSION==null or $_SESSION['type']!='staff member')
+//{
+//   header("Location: /layout/Appology/user-appology.html");
+//   exit();
+//}
+
+?>
+
+    <br>
+    <h1>Send Email</h1>
+    <form action="/./Database-Project/php/Staff-Member-Controller/send-email.php" method="post">
+        <label>To:</label>
+        <input name="recipient" type="text">
+
+        <br>
+
+        <label>Subject:</label>
+        <input name="subject" type="text">
+
+        <br>
+
+        <label>Body:</label>
+        <textarea name="body" rows="5" cols="50"></textarea>
+
+        <br>
+
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
--- php/navbar.php
+++ php/navbar.php
@@ -1,5 +1,5 @@
 <?php
-require("../helper/sqlExec.php");
+require($_SERVER['DOCUMENT_ROOT']."/Database-Project/helper/sqlExec.php");
 if(session_status() == PHP_SESSION_NONE)
 if($_SESSION['userid'] == null)
   session_start();
@@ -10,9 +10,9 @@ echo '<div id="navigation" class="col-xs-12">
         <a href="MainPage.php" class="navbar-brand">iWork</a>
           <a href="profile.php" class="navbar-brand">Profile</a>';
 if($_SESSION['userid'] != null)
-  echo '<a href="/Database-Project/php/logout.php" class="navbar-brand">Logout</a>';
-echo   '</div>
+  echo '<a href= "'.$_SERVER["DOCUMENT_ROOT"].'"/Database-Project/php/logout.php" class="navbar-brand">Logout</a>';
+echo   "</div>
     </div>
   </nav>
-</div>';
+</div>";
  ?>
