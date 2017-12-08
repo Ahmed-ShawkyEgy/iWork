a18d9dc2aaf1fab4b92f188b80783d13af18bf86
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-12-08 13:10:38 +0200

    fixed styling issues

--- php/navbar.php
+++ php/navbar.php
@@ -1,16 +1,22 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT']."/Database-Project/helper/sqlExec.php");
+
 if(session_status() == PHP_SESSION_NONE)
-if($_SESSION['userid'] == null)
-  session_start();
+    session_start();
+
 echo '<div id="navigation" class="col-xs-12">
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-      <div id="ShortcutLinks">
-        <a href="MainPage.php" class="navbar-brand">iWork</a>
-          <a href="profile.php" class="navbar-brand">Profile</a>';
-if($_SESSION['userid'] != null)
-  echo '<a href= "'.$_SERVER["DOCUMENT_ROOT"].'"/Database-Project/php/logout.php" class="navbar-brand">Logout</a>';
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div id="ShortcutLinks">
+                    <a href="MainPage.php" class="navbar-brand">iWork</a>
+                        <a href="profile.php" class="navbar-brand">Profile</a>';
+
+
+if(array_key_exists ('userid',$_SESSION))
+{ 
+    echo '<a href= "../Database-Project/php/logout.php" class="navbar-brand">Logout</a>';
+}
+   
 echo   "</div>
     </div>
   </nav>
